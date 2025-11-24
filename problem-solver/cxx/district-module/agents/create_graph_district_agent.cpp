5c065fb8a1a84d0d20eb83730499bea2287923cb
Artemdjdj <artembasuraa@gmail.com>
2025-11-24 16:25:52 +0300

    problems with agent initialisation

--- problem-solver/cxx/district-module/agents/create_graph_district_agent.cpp
+++ problem-solver/cxx/district-module/agents/create_graph_district_agent.cpp
@@ -30,45 +30,45 @@ ScResult CreateGraphAgent::DoProgram(ScActionInitiatedEvent const & event, ScAct
   m_context.GetLinkContent(elementAddr, scv_file_data);
 
   m_logger.Debug("We get path and ready to create graph\n");
-  m_logger.Debug(scv_file_data);
-
-  auto res = StringFormatter::Split(scv_file_data, '\n');
-  for (auto const & str : res)
+  ScStructure city = m_context.GenerateStructure();
+  auto resOfSplit = StringFormatter::Split(scv_file_data, '\n');
+  for (auto const & district : resOfSplit)
   {
-    m_logger.Debug(str);
-    // auto const & startDistrict = "Центральный";
-    // auto const & endDistrict = "Кировский";
-    // std::string const typeOfRoute{"автобус №5"};
+    m_logger.Debug(district);
+    auto const & district_elements = StringFormatter::Split(district, ';');
+    auto const & startDistrict = district_elements[0];
+    auto const & endDistrict = district_elements[1];
+    auto const & typeOfRoute = district_elements[2];
 
-    // ScAddr start_district_node = m_context.GenerateNode(ScType::ConstNode);
-    // m_context.SetElementSystemIdentifier(startDistrict, start_district_node);
+    ScAddr start_district_node = m_context.GenerateNode(ScType::ConstNode);
+    m_context.SetElementSystemIdentifier(startDistrict, start_district_node);
 
-    // ScAddr end_district_node = m_context.GenerateNode(ScType::ConstNode);
-    // m_context.SetElementSystemIdentifier(endDistrict, end_district_node);
+    ScAddr end_district_node = m_context.GenerateNode(ScType::ConstNode);
+    m_context.SetElementSystemIdentifier(endDistrict, end_district_node);
 
-    // ScAddr road = m_context.GenerateConnector(ScType::ConstCommonEdge, start_district_node, end_district_node);
-    // ScAddr node_of_road = m_context.GenerateConnector(ScType::ConstPermPosArc, GraphKeynodes::nrel_road, road);
+    ScAddr road = m_context.GenerateConnector(ScType::ConstCommonEdge, start_district_node, end_district_node);
+    ScAddr node_of_road = m_context.GenerateConnector(ScType::ConstPermPosArc, GraphKeynodes::nrel_road, road);
 
-    // ScStructure route = m_context.GenerateStructure();
-    // route << start_district_node << end_district_node << road << node_of_road;
+    ScStructure route = m_context.GenerateStructure();
+    route << start_district_node << end_district_node << road << node_of_road;
 
-    // ScAddr type_of_route = m_context.GenerateNode(ScType::ConstNode);
-    // if (typeOfRoute.find("автобус") != std::string::npos)
-    // {
-    //   m_context.SetElementSystemIdentifier(GraphKeynodes::concept_bus_route, type_of_route);
-    // }
-    // else if (typeOfRoute.find("трамвай") != std::string::npos)
-    // {
-    //   m_context.SetElementSystemIdentifier(GraphKeynodes::concept_tram_route, type_of_route);
-    // }
-    // else
-    // {
-    //   m_context.SetElementSystemIdentifier(GraphKeynodes::concept_subway_route, type_of_route);
-    // }
+    ScAddr type_of_route = m_context.GenerateNode(ScType::ConstNode);
+    if (typeOfRoute.find("автобус") != std::string::npos)
+    {
+      m_context.SetElementSystemIdentifier(GraphKeynodes::concept_bus_route, type_of_route);
+    }
+    else if (typeOfRoute.find("трамвай") != std::string::npos)
+    {
+      m_context.SetElementSystemIdentifier(GraphKeynodes::concept_tram_route, type_of_route);
+    }
+    else
+    {
+      m_context.SetElementSystemIdentifier(GraphKeynodes::concept_subway_route, type_of_route);
+    }
 
-    // ScAddr route_belong = m_context.GenerateConnector(ScType::ConstPermPosArc, type_of_route, route);
+    ScAddr route_belong = m_context.GenerateConnector(ScType::ConstPermPosArc, type_of_route, route);
 
-    // city << route << type_of_route << route_belong;
+    city << route << type_of_route << route_belong;
   }
 
   return action.FinishSuccessfully();
