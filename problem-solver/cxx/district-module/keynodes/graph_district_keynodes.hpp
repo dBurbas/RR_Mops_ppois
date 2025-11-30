310cd4b07bb11f72a5ad0ae594450e709a261e9e
Artemdjdj <artembasuraa@gmail.com>
2025-11-30 16:35:01 +0300

    [feat] upgrade knowledge-base

--- problem-solver/cxx/district-module/keynodes/graph_district_keynodes.hpp
+++ problem-solver/cxx/district-module/keynodes/graph_district_keynodes.hpp
@@ -16,8 +16,8 @@ public:
   static inline ScKeynode const concept_ready_to_analyze_city_routes{
       "concept_ready_to_analyze_city_routes",
       ScType::ConstNodeClass};
-  static inline ScKeynode const nrel_result_connectivity{"nrel_result_connectivity", ScType::ConstNodeNonRole};
-  static inline ScKeynode const nrel_result_bridges{"nrel_result_bridges", ScType::ConstNodeNonRole};
+  // static inline ScKeynode const nrel_result_connectivity{"nrel_result_connectivity", ScType::ConstNodeNonRole};
+  // static inline ScKeynode const nrel_result_bridge{"nrel_result_bridge", ScType::ConstNodeNonRole};
   static inline ScKeynode const nrel_transport_net_diameter{"nrel_transport_net_diameter", ScType::ConstNodeNonRole};
   static inline ScKeynode const nrel_file_path{"nrel_file_path", ScType::ConstNodeNonRole};
   static inline ScKeynode const nrel_road{"nrel_road", ScType::ConstNodeNonRole};
